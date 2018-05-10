a68cc0e36da08e6ce1e7f9c0c9132a04c049efec
Eklou Amemassovor <amekley@gmail.com>
2018-05-10 07:47:33 +0000

    main admin ok

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Author, Project
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+class AuthorAdmin(SummernoteModelAdmin):
+    list_display = ('name','slug', 'city', 'profession',)
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Author, AuthorAdmin)
+
+
+class ProjectAdmin(SummernoteModelAdmin):
+    list_display = ('title','author', 'link','slug')
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Project, ProjectAdmin)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Author(models.Model):
@@ -21,6 +21,9 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('projects_authors', args=[self.slug,self.id])
+
 
 class Project(models.Model):
     """
@@ -31,3 +34,11 @@ class Project(models.Model):
     link=models.CharField(max_length=250)
     description=models.TextField()
     cover=models.ImageField(upload_to="covers")
+    slug=models.SlugField(max_length=250)
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('projects', args=[self.slug,self.author])
